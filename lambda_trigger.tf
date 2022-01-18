3e6c3b0b11d756ef33c05f91476e6b7a172486c3
Giovanni Baquero <giovancito113@gmail.com>
2022-01-18 15:04:50 -0500

    :sparkles: Adding lambda trigger

--- lambda_trigger.tf
+++ lambda_trigger.tf
@@ -6,5 +6,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/groups/"
 }
