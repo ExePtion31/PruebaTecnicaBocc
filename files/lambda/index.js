0c638f54849f6f3881d7a256e123dde579cae418
Giovanni Baquero <giovancito113@gmail.com>
2022-01-18 16:04:49 -0500

    :wrench: Modifying Lambda

--- files/lambda/index.js
+++ files/lambda/index.js
@@ -1,15 +1,15 @@
 const handler = async (event) => {
-    console.debug('Lambda ejecutada correctamente');
-    console.debug('Fin de ejecución');
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*"
-      },
-    body: JSON.stringify('Célula de ahorros')
-    }
-}
+  console.debug("Lambda ejecutada correctamente");
+  console.debug("Fin de ejecución");
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify("Célula de ahorros"),
+  };
+};
 
 exports.handler = handler;
