1152e4e4d7d786512164fa76fe9c4086677c41df
Giovanni Baquero <giovancito113@gmail.com>
2022-01-18 13:25:34 -0500

    :sparkles: Implement APIGW

--- api.tf
+++ api.tf
@@ -13,6 +13,6 @@ resource "aws_api_gateway_resource" "api_gateway_resource" {
 resource "aws_api_gateway_method" "api_gateway_get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.api_gateway_resource.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
