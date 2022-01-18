87a074741a6bc5930ef50405f6c57a3601ed65ca
Giovanni Baquero <giovancito113@gmail.com>
2022-01-18 13:24:09 -0500

    :sparkles: Implement APIGW

--- api.tf
+++ api.tf
@@ -3,3 +3,16 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
   description = "API Gateway for ${var.stack_id}"
   tags = local.common_tags
 }
+
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "groups"
+}
+
+resource "aws_api_gateway_method" "api_gateway_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
