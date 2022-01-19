5dbefbbc3b0ca5ccb380e1264a59c75cbeadafd4
Giovanni Baquero <giovancito113@gmail.com>
2022-01-18 20:07:54 -0500

    :wrench: Modifying Lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,29 @@
 exports.handler = async (event) => {
   console.debug("Lambda ejecutada correctamente");
-  const arrayGroups = event.body;
-  return buildResponse(arrayGroups);
+  const values = JSON.parse(event.body);
+  var suma = 0;
+  var sizes = [];
+  var arrayGroups = values["groups"].split(",").map(function (item) {
+    return parseInt(item, 10);
+  });
+
+  const mayor = Math.max(...arrayGroups);
+
+  for (let i = 0; i < arrayGroups.length; i++) {
+    suma += arrayGroups[i];
+  }
+
+  for (let index = mayor; index <= suma; index++) {
+    if (suma % index == 0) {
+      sizes.push(String(index));
+    }
+  }
+  const response = [
+    {
+      sizes: sizes.join(","),
+    },
+  ];
+  return buildResponse(JSON.stringify(response));
 };
 
 function buildResponse(body) {
